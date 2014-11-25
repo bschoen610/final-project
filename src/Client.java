97c5bf71b09c6e87a11ddc4066fba13ffe106aec
William Alexander <github.com@willalexander.me>
2014-11-25 15:59:51 -0800

    Add more GUI

---
+++ src/Client.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+
+public class Client extends JFrame {
+	private static final long serialVersionUID = 6463534731269380864L;
+	
+	public Client() {
+		this.add(new LoginPanel());
+		this.setSize(800, 600);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Client();
+	}
+}
